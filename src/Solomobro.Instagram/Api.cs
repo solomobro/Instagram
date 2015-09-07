2d578c75b0d19c210278b4335f0104b03fe9364a
dialalpha <dial.alpha@gmail.com>
2015-09-07 01:50:14 -0400

    make Api constructor internal - must be constructed from OAuth class

--- src/Solomobro.Instagram/Api.cs
+++ src/Solomobro.Instagram/Api.cs
@@ -5,6 +5,15 @@
     /// </summary>
     public class Api
     {
-        
+        private readonly string _clientId;
+        private readonly string _clientSecret;
+        private readonly string _accessToken;
+
+        internal Api(string clientId, string clientSecret, string accessToken)
+        {
+            _clientId = clientId;
+            _clientSecret = clientSecret;
+            _accessToken = accessToken;
+        }
     }
 }
