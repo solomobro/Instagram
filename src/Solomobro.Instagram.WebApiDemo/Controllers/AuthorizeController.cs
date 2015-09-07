da8b49cd1de292c43a7ae85cc26e91be7d399ae3
dialalpha <dial.alpha@gmail.com>
2015-09-07 00:40:03 -0400

    clean up AuthorizeController in anticipation of API refactor

--- src/Solomobro.Instagram.WebApiDemo/Controllers/AuthorizeController.cs
+++ src/Solomobro.Instagram.WebApiDemo/Controllers/AuthorizeController.cs
@@ -18,42 +18,7 @@ namespace Solomobro.Instagram.WebApiDemo.Controllers
         [Route("api/authorize")]
         public IHttpActionResult AuthorizeInstagram(HttpRequestMessage req)
         {
-            var clientId = Settings.Settings.InstaClientID;
-            var clientSecret = Settings.Settings.InstaClientSecret;
-            var redirectUrl = Settings.Settings.InstaRedirectUrl;
-            var authConfig = new OAuthConfig(clientId, clientSecret, redirectUrl);
-
-            using (var ctFactory = new CancellationTokenSource())
-            {
-                var ct = ctFactory.Token;
-                var api = new Api(new Redirector(req, ct), authConfig);
-                api.AuthorizeAsync().Wait((int)TimeSpan.FromMinutes(1).TotalMilliseconds, ct);
-                
-            }
-
-
-
-
             return Ok("");
-        }
-
-        private class Redirector : IInteractiveAuthorizationProvider
-        {
-            private readonly HttpRequestMessage _request;
-            private readonly CancellationToken _token;
-
-            public Redirector(HttpRequestMessage req, CancellationToken token)
-            {
-                _request = req;
-                _token = token;
-            }
-            public Task<HttpResponseMessage> ProcessAuthorizationAsync(Uri uri)
-            {
-                
-                var resp = new RedirectResult(uri, _request);
-                var foo =  resp.ExecuteAsync(_token);
-                return foo;
-            }
         }
     }
 }
