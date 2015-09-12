cd22bc530dcabc78e99cf18841c795b93f47a450
dialalpha <dial.alpha@gmail.com>
2015-09-11 21:13:56 -0400

    Add file IExplicitAuthenticator - was missing from solution after merge

---
+++ src/Solomobro.Instagram/Interfaces/IExplicitAuthenticator.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using Solomobro.Instagram.Entities;
+
+namespace Solomobro.Instagram.Interfaces
+{
+    internal interface IExplicitAuthenticator
+    {
+        ExplicitAuthResponse Authenticate(Uri authEndpoint, IEnumerable<KeyValuePair<string, string>> authParams);
+    }
+}
