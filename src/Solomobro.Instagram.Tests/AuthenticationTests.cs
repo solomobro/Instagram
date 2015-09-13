268bcda745305f21d9a259b7b1025eb4e3b7be8d
dialalpha <dial.alpha@gmail.com>
2015-09-13 02:21:40 -0400

    Add first basic unit test

---
+++ src/Solomobro.Instagram.Tests/AuthenticationTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using Solomobro.Instagram.Authentication;
+
+namespace Solomobro.Instagram.Tests
+{
+    [TestFixture]
+    class AuthenticationTests
+    {
+        const string ClientId = "CLIENT-ID";
+        const string ClientSecret = "CLIENT-SECRET";
+        const string RedirectUri = "REDIRECT-URI";
+
+        [Test]
+        public void DefaultAuthMethodIsExplicit()
+        {
+            var auth = new OAuth(ClientId, ClientSecret, RedirectUri);
+
+            Assert.That(auth.AuthMethod, Is.EqualTo(AuthenticationMethod.Explicit));
+        }
+
+    }
+}
