a41b503e88fcb8806048c7d82cc6b56b92266da0
dialalpha <dial.alpha@gmail.com>
2015-09-23 19:32:19 -0400

    Add class UnixTimeConverter

---
+++ src/Solomobro.Instagram/UnixTimeConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.SessionState;
+
+namespace Solomobro.Instagram
+{
+    class UnixTimeConverter
+    {
+        private static DateTime _nineteenSeventy = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+
+        public static DateTime ConvertFromUnixTime(long unixTime)
+        {
+            return _nineteenSeventy.AddSeconds(unixTime);
+        }
+
+        public static DateTime ConvertFromUnixTime(string unixTime)
+        {
+            return ConvertFromUnixTime(long.Parse(unixTime));
+        }
+    }
+}
