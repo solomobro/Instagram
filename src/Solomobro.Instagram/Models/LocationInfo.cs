e0a64dc0210ad7f7d3c56818beb69e7061b2ab09
dialalpha <dial.alpha@gmail.com>
2015-09-23 21:43:12 -0400

    Add models: location info and mediainfo

---
+++ src/Solomobro.Instagram/Models/LocationInfo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solomobro.Instagram.Models
+{
+    [DataContract]
+    public class LocationInfo
+    {
+        internal LocationInfo() { }
+
+        [DataMember(Name = "id")]
+        public long Id { get; internal set; }
+
+        [DataMember(Name = "name")]
+        public string Name { get; internal set; }
+
+        [DataMember(Name = "latitude")]
+        public int Latitude { get; internal set; }
+
+        [DataMember(Name = "longitude")]
+        public int Longitude { get; internal set; }
+    }
+}
---
+++ src/Solomobro.Instagram/Models/MediaInfo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Solomobro.Instagram.Models
+{
+    [DataContract]
+    class MediaInfo
+    {
+        internal MediaInfo() { }
+
+        public Uri Url => new Uri(UrlInternal);
+
+        [DataMember(Name = "url")]
+        internal string UrlInternal;
+
+        [DataMember(Name = "width")]
+        public int Width { get; internal set; }
+
+        [DataMember(Name = "height")]
+        public int Height { get; internal set; }
+    }
+}
