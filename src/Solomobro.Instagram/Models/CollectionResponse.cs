be4f4dcfa08151e23ff3ded55fc02d1eb5c5c012
dialalpha <dial.alpha@gmail.com>
2015-09-26 11:42:14 -0400

    addCcollectionResponse and ObjectResponse. Delete IPagination

---
+++ src/Solomobro.Instagram/Models/CollectionResponse.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Threading.Tasks;
+
+namespace Solomobro.Instagram.Models
+{
+    [DataContract]
+    class CollectionResponse<T> : Response
+    {
+        public IReadOnlyList<T> Data => DataInternal.AsReadOnly();
+
+        [DataMember(Name = "data")]
+        internal List<T> DataInternal;
+
+        public async Task<CollectionResponse<T>> GetNextResultAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Solomobro.Instagram/Models/ObjectResponse.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.Serialization;
+
+namespace Solomobro.Instagram.Models
+{
+    [DataContract]
+    class ObjectResponse<T> : Response
+    {
+        internal ObjectResponse() { }
+
+        [DataMember(Name = "data")]
+        public T Data { get; internal set; }
+    }
+}
