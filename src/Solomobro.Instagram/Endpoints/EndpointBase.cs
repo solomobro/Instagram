7ac3095b9bd1d011478e204cc0f490544c947db3
dialalpha <dial.alpha@gmail.com>
2015-10-25 00:20:44 -0400

    minor refactor in EndpointBase to match naming conventions

--- src/Solomobro.Instagram/Endpoints/EndpointBase.cs
+++ src/Solomobro.Instagram/Endpoints/EndpointBase.cs
@@ -11,29 +11,29 @@ namespace Solomobro.Instagram.Endpoints
 {
     public class EndpointBase
     {
-        private readonly IApiClient ApiClient;
+        private readonly IApiClient _apiClient;
         protected readonly string AccessToken;
         protected const string Self = "self";
 
         internal EndpointBase(string accessToken)
         {
             AccessToken = accessToken;
-            ApiClient = Ioc.Resolve<IApiClient>() ?? new ApiClient();
+            _apiClient = Ioc.Resolve<IApiClient>() ?? new ApiClient();
         }
 
         protected async Task<ObjectResponse<T>> GetObjectResponseAsync<T>(Uri uri)
         {
-            return await ApiClient.GetAsync<ObjectResponse<T>>(uri).ConfigureAwait(false);
+            return await _apiClient.GetAsync<ObjectResponse<T>>(uri).ConfigureAwait(false);
         }
 
         protected async Task<CollectionResponse<T>> GetCollectionResponseAsync<T>(Uri uri)
         {
-            return await ApiClient.GetAsync<CollectionResponse<T>>(uri).ConfigureAwait(false);
+            return await _apiClient.GetAsync<CollectionResponse<T>>(uri).ConfigureAwait(false);
         }
 
         protected async Task<ObjectResponse<T>> PostObjectResponseAsync<T>(Uri uri, HttpContent content)
         {
-            return await ApiClient.PostAsync<ObjectResponse<T>>(uri, content).ConfigureAwait(false);
+            return await _apiClient.PostAsync<ObjectResponse<T>>(uri, content).ConfigureAwait(false);
         }
 
         
