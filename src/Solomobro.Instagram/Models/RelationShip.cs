9d96187d2724a78d4a78035c08daf9f7d8e4d7b2
dialalpha <dial.alpha@gmail.com>
2015-12-25 12:39:25 -0500

    add relationship data contract

---
+++ src/Solomobro.Instagram/Models/RelationShip.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solomobro.Instagram.Models
+{
+    [DataContract]
+    public class RelationShip
+    {
+        internal RelationShip() { }
+
+        [DataMember(Name = "outgoing_status")]
+        public string OutgoingStatus { get; internal set; }
+
+        [DataMember(Name = "incoming_status")]
+        public string IncomingStatus { get; internal set; }
+    }
+}
