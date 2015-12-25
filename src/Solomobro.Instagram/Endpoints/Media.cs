d16a4b86e4611e40be648dff3303e05380c3b027
dialalpha <dial.alpha@gmail.com>
2015-12-25 18:59:11 -0500

    implement Media api

--- src/Solomobro.Instagram/Endpoints/Media.cs
+++ src/Solomobro.Instagram/Endpoints/Media.cs
@@ -1,6 +1,43 @@
-﻿namespace Solomobro.Instagram.Endpoints
+﻿using System;
+using System.Threading.Tasks;
+using Solomobro.Instagram.Models;
+
+namespace Solomobro.Instagram.Endpoints
 {
     public class Media
     {
+        private const string EndpointUri = "https://api.instagram.com/v1/media";
+        private readonly EndpointBase _endpointBase;
+        private readonly string _accessToken;
+
+        internal Media(EndpointBase endpoint, string accessToken)
+        {
+            _endpointBase = endpoint;
+            _accessToken = accessToken;
+        }
+
+        /// <summary>
+        /// Implements GET /media/{media-idea}
+        /// </summary>
+        public async Task<ObjectResponse<Post>> GetAsync(string mediaId)
+        {
+            var uri = new Uri($"{EndpointUri}/{mediaId}?access_token={_accessToken}");
+            return await _endpointBase.GetObjectResponseAsync<Post>(uri).ConfigureAwait(false);
+        }
+
+        /// <summary>
+        /// Implement GET /media/shortcode/{media-id}
+        /// </summary>
+        public async Task<ObjectResponse<Post>> GetWithShortCodeAsync(string shortCode)
+        {
+            var uri = new Uri($"{EndpointUri}/shortcode/{shortCode}?access_token={_accessToken}");
+            return await _endpointBase.GetObjectResponseAsync<Post>(uri).ConfigureAwait(false);
+        }
+
+        public async Task<CollectionResponse<Post>> Search(SearchMediaRequest req)
+        {
+            var uri = new Uri($"{EndpointUri}/search/?access_token={_accessToken}&lat={req.Latitude}&lng={req.Longitude}&distance={req.DistanceMeters}");
+            return await _endpointBase.GetCollectionResponseAsync<Post>(uri).ConfigureAwait(false);
+        } 
     }
 }
---
+++ src/Solomobro.Instagram/Models/SearchMediaRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solomobro.Instagram.Models
+{
+    public class SearchMediaRequest
+    {
+        public float Latitude { get; set; }
+        public float Longitude { get; set; }
+        public int DistanceMeters { get; set; }
+    }
+}
