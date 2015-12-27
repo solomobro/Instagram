d1e2e79fc8e715a1ada64e4a9279c2d120f3a0c5
dialalpha <dial.alpha@gmail.com>
2015-12-27 02:44:55 -0500

    // todo: add users_in_photo to Post datacontract

--- src/Solomobro.Instagram/Models/Post.cs
+++ src/Solomobro.Instagram/Models/Post.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.Serialization;
 
+// todo: add users in photo
+
 namespace Solomobro.Instagram.Models
 {
     [DataContract]
