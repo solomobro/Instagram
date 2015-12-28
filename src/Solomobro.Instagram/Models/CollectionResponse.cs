e44a0fb7f9657dc6e4aed03b80e0339afe8e5c2a
dialalpha <dial.alpha@gmail.com>
2015-12-28 15:05:07 -0500

    no need to do lazy initialization in CollectionResponse since ApiClient is now cheap to create and destroy, and doesn't leak HttpClient instances

--- src/Solomobro.Instagram/Models/CollectionResponse.cs
+++ src/Solomobro.Instagram/Models/CollectionResponse.cs
@@ -12,18 +12,7 @@ namespace Solomobro.Instagram.Models
     [DataContract]
     public class CollectionResponse<T> : IResponse, IEnumerable
     {
-        /// <summary>
-        /// We need this to be lazy because we don't want to risk instantiating
-        /// an http client unless we absolutely need to
-        /// </summary>
-        private readonly Lazy<IApiClient> _lazyApiClient;
-
-
-        internal CollectionResponse()
-        {
-            _lazyApiClient = new Lazy<IApiClient>(
-                () => Ioc.Resolve<IApiClient>() ?? new ApiClient());
-        }
+        internal CollectionResponse() { }
 
         public int Count => Data?.Count ?? 0;
 
@@ -51,7 +40,7 @@ namespace Solomobro.Instagram.Models
                 return null;
             }
 
-            var apiClient = _lazyApiClient.Value;
+            var apiClient = Ioc.Resolve<IApiClient>() ?? new ApiClient();
             return await apiClient.GetCollectionResponseAsync<T>(Pagination.NextUrl).ConfigureAwait(false);
         }
 
