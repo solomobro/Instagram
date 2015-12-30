88461bd5a31022334db67b6a0cdc66865ccf51b8
dialalpha <dial.alpha@gmail.com>
2015-12-30 02:42:56 -0500

    set WebApiDemo internals visible to test project

--- src/Solomobro.Instagram.WebApiDemo/Properties/AssemblyInfo.cs
+++ src/Solomobro.Instagram.WebApiDemo/Properties/AssemblyInfo.cs
@@ -14,6 +14,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
+// Set internals visibility to test proj
+[assembly: InternalsVisibleTo("Solomobro.Instagram.Tests")]
+
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
